010a0feef02de2926546c81e3b97cb508df86ebc
Konstantin <157764402+scuddi@users.noreply.github.com>
2025-01-10 14:52:44 +0100

    Add variables and operate function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Functionality for the calculator
 
+// Variables for math operations
+
+let var_1;
+let var_2;
+let operator;
+
 // Basic mathematical functions
 
 function add(a, b) {
@@ -18,3 +24,8 @@ function divide(a, b) {
     return a / b;
 };
 
+// Operate function
+
+function operate (operator, num1, num2) {
+    return add(num1, num2);
+};
