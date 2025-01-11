8c7e04e7d465c952679a33d8ea97f9c04ac2d8b9
Konstantin <157764402+scuddi@users.noreply.github.com>
2025-01-11 15:08:14 +0100

    Add function operatorClicked
    Added the function for the functionality of
    operators.
    
    When clicked, the previously entered numbers are
    put to the last-input field and the operator is
    put to the new-input field. Afterwards new numbers
    for the second operand can be entered.

--- script.js
+++ script.js
@@ -48,6 +48,13 @@ function updateDisplay (single_value) {
     };
 };
 
+// Function when operator is clicked
+
+function operatorClicked (operator_clicked) {
+    last_input.innerHTML = input_field.innerHTML;
+    input_field.innerHTML = operator_clicked;
+}
+
 // Function to clear calculator display
 
 function clearDisplay() {
@@ -57,6 +64,7 @@ function clearDisplay() {
 // Selectors for all buttons and others
 
 const input_field = document.querySelector("#new-input")
+const last_input = document.querySelector("#last-input")
 
 const button_clear = document.querySelector("#clear");
 const button_plus = document.querySelector("#plus");
@@ -75,15 +83,21 @@ const button_equal = document.querySelector("#equal");
 // const button_8 = document.querySelector("#b8")
 // const button_9 = document.querySelector("#b9")
 
-const digit_click = document.querySelectorAll(".digit")
+const digit_click = document.querySelectorAll(".digit");
 
-const clear_click = document.querySelector("#clear")
+const operator_click = document.querySelectorAll(".operator");
+
+const clear_click = document.querySelector("#clear");
 
 // Eventlisteners for all buttons with class digit
 
 digit_click.forEach((digit) => {
     digit.addEventListener("click", () => updateDisplay(digit.innerHTML))
-}) 
+});
+
+operator_click.forEach((operator) => {
+    operator.addEventListener("click",() => operatorClicked(operator.innerHTML))
+});
 
 clear_click.addEventListener("click", clearDisplay);
 
