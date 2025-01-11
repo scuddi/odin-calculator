69913a447f3a9e18bf4ac26c0afec00fe3ce645e
Konstantin <157764402+scuddi@users.noreply.github.com>
2025-01-11 12:01:06 +0100

    Add Eventlistener for digit 1 changing input
    Is only a try, only works with one digit.

--- script.js
+++ script.js
@@ -38,7 +38,9 @@ function operate (operator, num1, num2) {
     };
 };
 
-// Selectors for all buttons
+// Selectors for all buttons and others
+
+const input_field = document.querySelector("#input")
 
 const button_clear = document.querySelector("#clear");
 const button_plus = document.querySelector("#plus");
@@ -55,3 +57,10 @@ const button_6 = document.querySelector("#b6")
 const button_7 = document.querySelector("#b7")
 const button_8 = document.querySelector("#b8")
 const button_9 = document.querySelector("#b9")
+
+// Eventlisteners for all buttons
+
+button_1.addEventListener("click", () => {
+    var_1 = 1;
+    input_field.textContent = var_1;
+});
